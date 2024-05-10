181cbd6a8200225a16a6d3f3fb839bfb4f06025c
slaytrunks <slaytrunks@gmail.com>
2024-05-10 09:33:18 +0545

    added routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter , Routes,Route} from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Projects from "./pages/Projects"
+import Dashboard from "./pages/Dashboard"
+
 
 const App = () => {
   return (
-    <div className='bg-red-400'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/'  element={<Home/>}/>
+      <Route path='/about'  element={<About/>}/>
+      <Route path='/signin'  element={<Signin/>}/>
+      <Route path='/signup'  element={<Signup/>}/>
+      <Route path='/dashboard'  element={<Dashboard/>}/>
+      <Route path='/projects'  element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
